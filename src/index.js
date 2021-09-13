e26309fc08384e96272f10de341e3fe7d850affd
Sanjeev <sanjumassal@gmail.com>
2021-09-13 12:55:17 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
